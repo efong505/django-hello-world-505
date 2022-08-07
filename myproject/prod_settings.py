51a1599e68eb8e9ed3bd11cff2aa3f208dc3235c
Ed <65623961+efong505@users.noreply.github.com>
2022-08-06 18:51:10 -0600

    Update prod_settings.py

--- myproject/prod_settings.py
+++ myproject/prod_settings.py
@@ -8,7 +8,7 @@ import django_heroku
 DEBUG = False
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 # adjust to the URL of your Heroku app
-ALLOWED_HOSTS = ['django-hello-world-app.herokuapp.com']
+ALLOWED_HOSTS = ['django-hello-world-505.herokuapp.com']
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
